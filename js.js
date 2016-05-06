41971e5da49761e8de902484cbade2e29f8c9894
ginpei <ginpei@gmail.com>
2016-05-06 09:19:22 -0700

    add stacked area chart

--- js.js
+++ js.js
@@ -22,3 +22,38 @@
 		},
 	});
 })();
+
+// Stacked area chart
+(function() {
+	var ctx = document.querySelector('#stackedArea').getContext('2d');
+	var data = getData('line');  // (data[0].date instanceof Date === true)
+
+	var chart = new Chart(ctx, {
+		type: 'line',
+		data: {
+			labels: data.map(v=>`${v.date.getFullYear()}-${v.date.getMonth()+1}-${v.date.getDay()}`),
+			datasets: [
+				{
+					data: data.map(v=>(v.value+Math.random()*200-50)/2).reverse(),
+					backgroundColor: '#f90',
+					borderColor: '#f00',
+				},
+				{
+					data: data.map(v=>v.value),
+					backgroundColor: '#09f',
+					borderColor: '#00f',
+					label: 'My Data',
+				},
+			]
+		},
+		options: {
+			scales: {
+				yAxes: [
+					{
+						stacked: true
+					}
+				]
+			}
+		}
+	});
+})();
